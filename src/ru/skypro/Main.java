77fb5eb34e158ef9c0be27558989b0ad68ba2597
Ruslan <gimazevs@mail.ru>
2022-02-02 22:26:27 +0300

    выполнил 2 задание

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,12 +3,16 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	    byte clientOS = 0;
-        if ( clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
-        } else if ( clientOS == 1) {
-            System.out.println("Установите версию приложения для Android по ссылке");
-
+        byte clientOS2 = 1;
+        int clientDeviceYear = 2014;
+        if ( clientOS2 == 0 && clientDeviceYear <= 2014) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (clientOS2 == 1 && clientDeviceYear <= 2014) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите обычную версию");
         }
+        }
+
     }
-}
+
