da03779fc977c48a85e799ed09a004417ec20924
Ruslan <gimazevs@mail.ru>
2022-02-02 21:16:00 +0300

    второй коммит

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,12 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    byte clientOS = 0;
+        if ( clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        } else if ( clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+
+        }
     }
 }
