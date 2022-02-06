f0e96edd748e00e4511602fff311ac00e21a56a6
Ruslan <gimazevs@mail.ru>
2022-02-06 10:09:07 +0300

    5 задание, исправил сезоны, проверьте

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         byte clientOS = 1;
-        if ( clientOS == 0) {
+        if (clientOS == 0) {
             System.out.println("Установите версию приложения для iOS по ссылке");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Андроид по ссылке");
@@ -21,67 +21,50 @@ public class Main {
         }
 
         int year = 1000;
-        if (( year % 4 == 0) && ((year % 100 != 0) || year % 400 == 0)) {
+        if ((year % 4 == 0) && ((year % 100 != 0) || year % 400 == 0)) {
             System.out.println(year + " високосный год");
         } else {
-            System.out.println( year + " не является високосным годом");
+            System.out.println(year + " не является високосным годом");
         }
 
         int deliveryDistance = 99;
         if (deliveryDistance < 20) {
-        System.out.println("Потребуется дней: 1 день");
-    } else {
-        if (deliveryDistance >= 20 && deliveryDistance < 60) {
-            System.out.println("Потребуется дней: 2 день");
-        } else {
-            if (deliveryDistance >= 60 && deliveryDistance < 100) {
-                System.out.println("Потребуется дней: 3 дня");
-            }
+            System.out.println("Потребуется дней: 1 день");
+        } else if (deliveryDistance < 60) {
+            System.out.println("Потребуется дней: 2 дня");
+        } else if (deliveryDistance < 100) {
+            System.out.println("Потребуется дней: 3 дня");
         }
-    }
-        int monthNumber = 13;
+
+        int monthNumber = 8;
         switch (monthNumber) {
             case 1:
-                System.out.println("Январь - это зима");
-                break;
             case 2:
-                System.out.println("Февраль - это зима");
+            System.out.println("Сезон - зима");
                 break;
             case 3:
-                System.out.println("Март - это весна");
-                break;
             case 4:
-                System.out.println("Апрель - это весна");
-                break;
             case 5:
-                System.out.println("Май - это весна");
+            System.out.println("Сезон - весна");
                 break;
             case 6:
-                System.out.println("Июнь - это лето");
-                break;
             case 7:
-                System.out.println("Июль - это лето");
-                break;
             case 8:
-                System.out.println("Август - это лето");
+                System.out.println("Сезон - это лето");
                 break;
             case 9:
-                System.out.println("Сентябрь - это осень");
-                break;
             case 10:
-                System.out.println("Октябрь - это осень");
-                break;
             case 11:
-                System.out.println("Ноябрь - это осень");
+                System.out.println("Сезон - это осень");
                 break;
             case 12:
-                System.out.println("Декабрь - это зима");
+                System.out.println("Сезон - это зима");
                 break;
             default:
                 System.out.println("Такого месяца не сущесвует");
         }
     }
-    }
+}
 
 
 
